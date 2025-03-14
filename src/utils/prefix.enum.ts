409369baf6404ce6a1695fb58bdc057bf1097f43
Nikulin Artem <artem.nikulin.0526@gmail.com>
2025-03-14 20:43:46 +0200

    feat(15): add endpoints for calendar user model

--- src/utils/prefix.enum.ts
+++ src/utils/prefix.enum.ts
@@ -5,6 +5,7 @@ export enum Prefix {
   USERS = 'users',
   CALENDARS = 'calendars',
   CALENDAR_INVITATIONS = 'calendar-invitations',
+  CALENDAR_USER = 'calendar-users',
   ASSETS = 'assets',
 }
 export const GLOBAL_PREFIX = 'api';
